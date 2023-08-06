40a58449c10d85cc88f7653dcfe5095bcbd2753a
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-08-06 21:09:20 +0200

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Kirill Streltsov on 06.08.23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
